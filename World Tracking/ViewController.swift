1897ffb33e31fe9598577ba33824af5ec974a045
devbaggett <devabaggett@gmail.com>
2018-05-04 13:13:12 -0700

    changes made to world origin

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -31,7 +31,7 @@ class ViewController: UIViewController {
         let node = SCNNode()
         node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
         node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(-0.3,-0.2,-0.3)
+        node.position = SCNVector3(0,0,-0.3)
         self.sceneView.scene.rootNode.addChildNode(node)
     }
     
