17cb8ef3c2903f9b14557933897fc389fba874e7
devbaggett <devabaggett@gmail.com>
2018-05-24 15:56:54 -0700

    converted degrees to radians

--- World Tracking/ViewController.swift
+++ World Tracking/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController {
         self.sceneView.debugOptions = [ARSCNDebugOptions.showFeaturePoints, ARSCNDebugOptions.showWorldOrigin]
         // track position and orientation at all times
         self.sceneView.session.run(configuration)
+        self.sceneView.autoenablesDefaultLighting = true
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -28,11 +29,56 @@ class ViewController: UIViewController {
     }
 
     @IBAction func add(_ sender: Any) {
+//        let cylinder = SCNNode(geometry: SCNCylinder(radius: 0.1, height: 0.15))
+//        cylinder.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
+//        cylinder.position = SCNVector3(0,0,-0.3)
+//        cylinder.eulerAngles = SCNVector3(0,0,Float(90.degreesToRadians))
+//        self.sceneView.scene.rootNode.addChildNode(cylinder)
+//
+//        let pyramid = SCNNode(geometry: SCNPyramid(width: 0.1, height: 0.1, length: 0.1))
+//        pyramid.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+//        pyramid.position = SCNVector3(0,0,-0.5)
+//        cylinder.addChildNode(pyramid)
+        
+        
+//        node.geometry = SCNCapsule(capRadius: 0.1, height: 0.3)
+//        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0.03)
+//        node.geometry = SCNCone(topRadius: 0.1, bottomRadius: 0.1, height: 0.3)
+//        node.geometry = SCNSphere(radius: 0.1)
+//        node.geometry = SCNCylinder(radius: 0.2, height: 0.2)
+//        node.geometry = SCNTube(innerRadius: 0.2, outerRadius: 0.3, height: 0.5)
+//        node.geometry = SCNTorus(ringRadius: 0.3, pipeRadius: 0.1)
+//        node.geometry = SCNPlane(width: 0.2, height: 0.2)
+//        let path = UIBezierPath()
+//        path.move(to: CGPoint(x: 0, y: 0))
+//        path.addLine(to: CGPoint(x: 0, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.2, y: 0.3))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0.2))
+//        path.addLine(to: CGPoint(x: 0.4, y: 0))
+//        let shape = SCNShape(path: path, extrusionDepth: 0.2)
+//        node.geometry = shape
+        
+        let doorNode = SCNNode(geometry: SCNPlane(width: 0.03, height: 0.06))
+        doorNode.geometry?.firstMaterial?.diffuse.contents = UIColor.brown
+        let boxNode = SCNNode(geometry: SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0))
+        boxNode.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
         let node = SCNNode()
-        node.geometry = SCNBox(width: 0.1, height: 0.1, length: 0.1, chamferRadius: 0)
-        node.geometry?.firstMaterial?.diffuse.contents = UIColor.blue
-        node.position = SCNVector3(0,0,-0.3)
+        node.geometry = SCNPyramid(width: 0.1, height: 0.1, length: 0.1)
+        node.geometry?.firstMaterial?.specular.contents = UIColor.orange
+        node.geometry?.firstMaterial?.diffuse.contents = UIColor.red
+        node.position = SCNVector3(0.2,0.3,-0.2) // (x,y,z)
+        node.eulerAngles = SCNVector3(Float(180.degreesToRadians),0,0)
+        boxNode.position = SCNVector3(0, -0.05, 0)
+        doorNode.position = SCNVector3(0, -0.02, 0.053)
         self.sceneView.scene.rootNode.addChildNode(node)
+        node.addChildNode(boxNode)
+        boxNode.addChildNode(doorNode)
+//        let x = randomNumbers(firstNum: -0.1, secondNum: 0.1)
+//        let y = randomNumbers(firstNum: -0.1, secondNum: 0.1)
+//        let z = randomNumbers(firstNum: -0.1, secondNum: 0.1)
+
+       
+//        self.sceneView.scene.rootNode.addChildNode(cylinderNode)
     }
     
     @IBAction func reset(_ sender: Any) {
@@ -47,5 +93,12 @@ class ViewController: UIViewController {
         }
         self.sceneView.session.run(configuration, options: [.resetTracking, .removeExistingAnchors])
     }
+    func randomNumbers(firstNum: CGFloat, secondNum: CGFloat) -> CGFloat {
+        return CGFloat(arc4random()) / CGFloat(UINT32_MAX) * abs(firstNum - secondNum) + min(firstNum, secondNum)
+    }
+}
+
+extension Int {
+    var degreesToRadians: Double { return Double(self) * .pi/180}
 }
 
